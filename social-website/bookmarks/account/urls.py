c420fd46dea22c14b00761c3979759a73e02aa3a
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2023-01-07 16:59:05 +0100

    feat: add password change operations

--- social-website/bookmarks/account/urls.py
+++ social-website/bookmarks/account/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+)
 from . import views
 
 urlpatterns = [
@@ -7,4 +12,11 @@ urlpatterns = [
     path("login-dj/", LoginView.as_view(), name="dj_login"),
     path("logout-dj/", LogoutView.as_view(), name="dj_logout"),
     path("profile/", views.personal_account, name="personal"),
+    # change password urls
+    path("password_change/", PasswordChangeView.as_view(), name="password_change"),
+    path(
+        "password_change/done/",
+        PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
